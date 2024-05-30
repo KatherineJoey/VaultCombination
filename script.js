3ec6f583d068c3c19bb89f264dbf83c8290be779
Katherine Joey Arambulo <168380621+KatherineJoey@users.noreply.github.com>
2024-05-30 16:53:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Pseudocode: 
+// 1. Create a string message for the user.
+// 2. Define the variables to generate the secret combination numbers.
+// 3. Generate the first combination using subtraction.
+// 4. Generate the second combination using addition.
+// 5. Generate the third combination using multiplication.
+// 6. Display the combination in an alert.
+
+const Message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+const num1 = 15 - 5; // The result will be equals to 10
+const num2 = 30 + 10; // The result will be equals to 40
+const num3 = 13 * 3; // The result will be equals to 39
+
+alert("Warning:  " + Message + "  " + num1 + " - " + num2 + " - " + num3);
+
+//I am still a bit confused but I think I will be okay with a few practices :)
